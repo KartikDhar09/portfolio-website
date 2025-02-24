3d2d154e146299e0fec4ec3f281a89aafcf1f77c
kartiK <kartikdhar1309@gmail.com>
2025-02-24 21:58:00 +0530

    projects added

--- src/components/pages/project/projects.js
+++ src/components/pages/project/projects.js
@@ -1,12 +1,11 @@
 import taskManager from "./imgs_demo/task-management-dashboard.png";
 import rmax from "./imgs_demo/rmax-website.png";
 import workflowCanvas from "./imgs_demo/workflow-canvas.png";
+import portfolio from './imgs_demo/portfolioDemo.png'
 export const projects = [
   {
     name: "Task-Management-Dashboard",
     image: taskManager,
-    description: `Modern task management web application using React, Redux, and TailwindCSS with features like
-authentication, dashboard analytics, and real-time task tracking integrated with Appwrite backend services.`,
     githubLink: "https://github.com/KartikDhar09/task-management-dashboard",
     liveLink: "https://task-management-dashboard-amber-six.vercel.app",
    technologies: ['React', 'Redux', 'Redux Thunk', 'React Context API', 'Tailwind CSS', 'Recharts',
@@ -16,9 +15,7 @@ authentication, dashboard analytics, and real-time task tracking integrated with
   {
     name: "Rmax-Website",
     image: rmax,
-    description: `Modern React web application with advanced UI components, form validation, and multi-page routing
-using TailwindCSS, React Router, and shadcn/ui components.`,
-    githubLink: "https://github.com/KartikDhar09/RMax-SaaS-Platform-Website",
+   githubLink: "https://github.com/KartikDhar09/RMax-SaaS-Platform-Website",
     liveLink: "https://rmax-saas-platform.netlify.app/",
     technologies: [ 'React', 'Tailwind CSS',' React Router', 'React Hook Form', 'Git', 'Github', 'Responsive Design']
 
@@ -26,11 +23,16 @@ using TailwindCSS, React Router, and shadcn/ui components.`,
   {
     name: "Workflow-Canvas",
     image: workflowCanvas,
-    description: `React-based interactive workflow editor for creating, visualizing and managing flowcharts with
-drag-and-drop nodes and modern UI components.`,
     githubLink: "https://github.com/KartikDhar09/workflow-canvas",
     liveLink: "https://workflow-canvas.vercel.app/",
     technologies: [ 'React' , 'Tailwind CSS', 'Redux', 'ReactFlow' , 'Shadcn/ui', 'Git', 'Github']
+
+  },
+  {
+    name: "portfolio-website",
+    image: portfolio,
+    githubLink: "https://github.com/KartikDhar09/portfolio-website",
+    technologies: [ 'React', 'Emailjs' ,  'Tailwind CSS', 'Responsive Design', 'Shadcn/ui', 'Git', 'Github']
 
   },
 ];
